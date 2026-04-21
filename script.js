cfff5a40f1bd6bd9779332c9902d702f14c82baf
Lillkvisten <lukpasve@gmail.com>
2026-04-21 21:01:13 +0200

    Did some stuff

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 function myFunction() {
     var elem = document.body;
-    elem.classList.toggle("dark-mode");
     var button = document.querySelector(".e");
-    button.innerHTML = elem.classList.contains("dark-mode") ? "Light Mode" : "Dark Mode";
+    
+    elem.classList.toggle("dark-mode");
+    var isDarkMode = elem.classList.contains("dark-mode");
+    button.innerHTML = isDarkMode ? "Light Mode" : "Dark Mode";
+    setCookie("theme", isDarkMode ? "dark" : "light", 30);
 }
 
 function changeFont(fontFamily) {
     document.body.style.fontFamily = fontFamily;
 }
+
+window.onload = function() {
+    var savedTheme = getCookie("theme");
+    var elem = document.body;
+    var button = document.querySelector(".e");
+
+    if (savedTheme === "dark") {
+        elem.classList.add("dark-mode");
+        if (button) button.innerHTML = "Light Mode";
+    }
+};
